41f18fda9dff27b31030a87f1473fdebb3deff94
steve-alloy <98038063+steve-alloy@users.noreply.github.com>
2022-09-29 21:47:08 -0400

    Change parseInt to parseFloat for sigma score

--- index.ts
+++ index.ts
@@ -15,23 +15,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post("/", (req: Request, res: Response) => {
-    console.log(req);
+
     const attributes = req.body[ACCOUNT_NAME + " Meta"];
     const dataSuplied = req.body[ACCOUNT_NAME + " Data Supplied"];
     const suppliedValue = req.body[ACCOUNT_NAME + " Supplied Value"];
     const socureData = req.body["Socure3"];
 
-    const sigmaFraudScore: number = parseInt(socureData.find((obj: Scores) => obj.attributeName === "Sigma Fraud Score").attributeValue);
+    const sigmaFraudScore: number = parseFloat(socureData.find((obj: Scores) => obj.attributeName === "Sigma Fraud Score").attributeValue);
     const otherRiskScore: number = parseInt(attributes.find((obj: Scores) => obj.attributeName === "other_data_risk_score").attributeValue);
 
     const modelScore: number = sigmaFraudScore * otherRiskScore;
 
-    console.log("Sigma Fraud:", sigmaFraudScore);
-    console.log("Other:", otherRiskScore);
     console.log("Model Score:", modelScore);
     console.log("Account Data Supplied:", dataSuplied);
     console.log("Supplied Value:", suppliedValue);
-
 
     res.json({
         modelScore,
